a18324503f8f6c512e8cf268832ef612e95e6b5d
guomaoqiu@sina.com <guomaoqiu@sina.com>
2018-06-12 13:12:11 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,57 @@
+#-*- coding:utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # send mail
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = '2399447849@qq.com' #os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'Guomaoqiu.310963' #os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = u''
+    FLASKY_MAIL_SENDER = '2399447849@qq.com'
+    FLASKY_ADMIN = '2399447849@qq.com' # os.environ.get('FANXIANG_ADMIN')
+
+
+    #文件上传
+ 
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    db_host = '127.0.0.1'
+    db_user = 'xiangce'
+    db_pass = 'xiangce'
+    db_name = 'xiangce'
+    SQLALCHEMY_DATABASE_URI = 'mysql://' + db_user + ':' + db_pass + '@' + db_host + '/' + db_name
+    SQLALCHEMY_ECHO=False #用于显式地禁用或启用查询记录
+
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
